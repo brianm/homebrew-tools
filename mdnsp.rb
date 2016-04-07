5b47cda24612c9022db02137fc1b8216da89b530
Brian McCallister <brianm@skife.org>
2016-04-07 16:58:26 -0700

    update mdnsp to 0.2.0

--- mdnsp.rb
+++ mdnsp.rb
@@ -4,20 +4,13 @@ require "language/go"
 class Mdnsp < Formula
     url "https://github.com/brianm/mdnsp.git",
       :using => :git,
-      :tag => "0.1.0"
-  version "0.1.0"
+      :tag => "0.2.0"
+  version "0.2.0"
   homepage "https://github.com/brianm/mdnsp"
   sha1 ""
 
   depends_on "go" => :build
-
-  go_resource "github.com/brianm/mdns" do
-    url "https://github.com/brianm/mdns.git", :revision => "0.1.0"
-  end
-
-  go_resource "github.com/miekg/dns" do
-    url "https://github.com/miekg/dns.git", :revision => "c2d8398a252a98a7ba29676e6946faf33c5e2fe1"
-  end
+  depends_on "godep" => :build
 
   def install
     ENV["GOPATH"] = buildpath
@@ -28,7 +21,7 @@ class Mdnsp < Formula
     Language::Go.stage_deps resources, buildpath/"src"
 
     cd my_path do
-      system "go", "build", "-o", "mdnsp", "./main.go"
+      system "godep", "go", "build"
     end
 
     bin.install my_path/"mdnsp"
