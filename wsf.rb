36f5ca2b3140f397ec2b29a9163dc794b123ce42
Brian McCallister <brianm@skife.org>
2016-04-27 16:23:08 -0700

    update wsf to 0.1.2

--- wsf.rb
+++ wsf.rb
@@ -3,10 +3,10 @@ require "formula"
 class Wsf < Formula
   head "https://github.com/brianm/wsf.git"
 
-  url "https://github.com/brianm/wsf/archive/wsf-0.1.1.tar.gz"
-  version "0.1.1"
+  url "https://github.com/brianm/wsf/archive/wsf-0.1.2.tar.gz"
+  version "0.1.2"
   homepage "https://github.com/brianm/wsf"
-  sha256 "850cc9b1a90c7ade14a100a78e8124e10a22c9860c985d31899037b03d5d7ce5"
+  sha256 ""
 
   depends_on "rust" => :build
   depends_on "pandoc" => :build
