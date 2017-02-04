ab107f0fa2732a456f2e1ce562c3476b3824eefd
Brian McCallister <brianm@skife.org>
2017-02-04 13:09:43 -0800

    wsf-0.1.6

--- wsf.rb
+++ wsf.rb
@@ -1,25 +1,24 @@
 require "formula"
 
-$wsf_version = "0.1.5"
+$pkg_version = "0.1.6"
+$pkg_group = "brianm"
+$pkg_repo = "wsf"
+$pkg_tag = "#{$pkg_repo}-#{$pkg_version}"
 
 class Wsf < Formula
-  head "https://github.com/brianm/wsf.git"
+  head "https://github.com/#{$pkg_group}/#{$pkg_repo}.git"
 
-  url "https://github.com/brianm/wsf/archive/wsf-#{$wsf_version}.tar.gz"
-  version $wsf_version
-  homepage "https://github.com/brianm/wsf"
-  sha256 "895afa3d4c1371f8b5b0cb57e4a361b00867b31960afc2c79942b673bdfbc7bb"
+  url "https://github.com/#{$pkg_group}/#{$pkg_repo}/archive/#{$pkg_tag}.tar.gz"
+  version $pkg_version
+  homepage "https://github.com/#{$pkg_group}/#{$pkg_repo}"
+  sha256 ""
 
   depends_on "rust" => :build
   depends_on "pandoc" => :build
   depends_on "openssl"
 
   def install
-    # export OPENSSL_INCLUDE_DIR=`/include
-    # export OPENSSL_LIB_DIR=`brew --prefix openssl`/lib
-    #ENV['OPENSSL_INCLUDE_DIR'] = "#{`brew --prefix openssl`}/include"
-    #ENV['OPENSSL_LIB_DIR'] = "#{`brew --prefix openssl`}/lib"
-    system "make"
+    system "cargo build --release"
     bin.install "target/release/wsf"
     man1.install "wsf.1"
   end
