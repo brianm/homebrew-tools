a8b693c4cbc0e72a6653b18522890d4d41a85f99
Brian McCallister <brianm@skife.org>
2017-02-04 16:02:12 -0800

    jp-0.3.1

--- jp.rb
+++ jp.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-$jp_version = "0.3.0"
+$jp_version = "0.3.1"
 
 class Jp < Formula
   head "https://github.com/brianm/jp.git"
@@ -8,7 +8,7 @@ class Jp < Formula
   url "https://github.com/brianm/jp/archive/jp-#{$jp_version}.tar.gz"
   version $wsf_version
   homepage "https://github.com/brianm/jp"
-  sha256 "5fa4926421005f88989a5d4bb5a80d15167e7e88b1454402fec2db059ab5de86"
+  sha256 "478bb7b1f5c083f4f9e3a9f59491d53c5ec1a4085bd837df82bdbb63f5acb474"
 
   depends_on "rust" => :build
   depends_on "pandoc" => :build
