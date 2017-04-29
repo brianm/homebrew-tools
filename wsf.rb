087b62cafa0c0857cb28be636b30202085e00d2d
Brian McCallister <brianm@skife.org>
2017-04-28 17:50:17 -0700

    wsf-0.2.1

--- wsf.rb
+++ wsf.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-$pkg_version = "0.1.6"
+$pkg_version = "0.2.1"
 $pkg_group = "brianm"
 $pkg_repo = "wsf"
 $pkg_tag = "#{$pkg_repo}-#{$pkg_version}"
@@ -11,7 +11,7 @@ class Wsf < Formula
   url "https://github.com/#{$pkg_group}/#{$pkg_repo}/archive/#{$pkg_tag}.tar.gz"
   version $pkg_version
   homepage "https://github.com/#{$pkg_group}/#{$pkg_repo}"
-  sha256 ""
+  sha256 "5518593084a91a2a65f19a43053c500ec8c1268c85f86beff34aef69954ebd3f"
 
   depends_on "rust" => :build
   depends_on "pandoc" => :build
