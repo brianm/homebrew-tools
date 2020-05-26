b11c2c3a386d538ffbe409d346e6b621509963ba
Brian McCallister <bmccallister@groupon.com>
2020-05-26 11:31:58 -0700

    update dice to 0.3.0

--- dice.rb
+++ dice.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-$pkg_version = "0.2.0"
+$pkg_version = "0.3.0"
 $pkg_group = "brianm"
 $pkg_repo = "dice"
 $pkg_tag = "#{$pkg_repo}-#{$pkg_version}"
@@ -11,7 +11,7 @@ class Dice < Formula
   url "https://github.com/#{$pkg_group}/#{$pkg_repo}/archive/#{$pkg_tag}.tar.gz"
   version $pkg_version
   homepage "https://github.com/#{$pkg_group}/#{$pkg_repo}"
-  sha256 "9a5372514179639a2144a66dea4943278830dbae15d817b2c2ffc11fe7b12cbf"
+  sha256 "e73677254819e467e587c292a2932947e0419d1dc10f2386d5233bbdf1ad223f"
 
   depends_on "rust" => :build
 
