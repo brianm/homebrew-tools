e89b14edaf08a60a39404806d712549968c217c8
Brian McCallister <brianm@skife.org>
2025-11-26 19:49:46 -0800

    working on bt

--- bt.rb
+++ bt.rb
@@ -3,7 +3,7 @@ require "formula"
 $pkg_version = "0.1.0"
 $pkg_group = "brianm"
 $pkg_repo = "bt"
-$pkg_tag = "#{$pkg_repo}-#{$pkg_version}"
+$pkg_tag = "v#{$pkg_version}"
 
 class Bt < Formula
   head "https://github.com/#{$pkg_group}/#{$pkg_repo}.git"
@@ -11,7 +11,7 @@ class Bt < Formula
   url "https://github.com/#{$pkg_group}/#{$pkg_repo}/archive/#{$pkg_tag}.tar.gz"
   version $pkg_version
   homepage "https://github.com/#{$pkg_group}/#{$pkg_repo}"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "9939a48cf0259c11ad8d57b8ca0d3f2b1643b21ed81b50492a0f88999633a0a4"
 
   depends_on "rust" => :build
 
