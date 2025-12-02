ef1821992bf94e287af9bf1ef44399d268586724
Brian McCallister <brianm@skife.org>
2025-12-01 17:33:22 -0800

    bt to 0.3.0

--- bt.rb
+++ bt.rb
@@ -1,8 +1,8 @@
 class Bt < Formula
   desc "Small task tracker for small projects"
   homepage "https://github.com/brianm/bt"
-  url "https://github.com/brianm/bt/archive/refs/tags/v0.2.2.tar.gz"
-  sha256 "a489bfe3875f9f9e66e0969c4bda703c529796769f434b07213279bbb4aa4659"
+  url "https://github.com/brianm/bt/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "df2eac02485c88fdace629fd7d581372f75938e966424d4eb6e3e9608795aaa8"
   license "Apache-2.0"
   head "https://github.com/brianm/bt.git"
 
