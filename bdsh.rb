19f9230dc35273226ba8d66b9d86e05645273a70
Brian McCallister <brianm@skife.org>
2026-01-27 07:15:15 -0800

    Fix bdsh SHA256 for proper git tag

--- bdsh.rb
+++ bdsh.rb
@@ -2,7 +2,7 @@ class Bdsh < Formula
   desc "Better Distributed Shell - run commands on multiple hosts with consensus view"
   homepage "https://github.com/brianm/bdsh"
   url "https://github.com/brianm/bdsh/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "eba78f11df453bb8bcef52bfa7f8bdd0a3767ec9ca3c613e77c11890b6b98e69"
   license "Apache-2.0"
   head "https://github.com/brianm/bdsh.git"
 
